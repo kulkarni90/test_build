709e84492b408bed013a174c7f533b86d56b69d6
phaneendra kulkarni <68918128+kulkarni90@users.noreply.github.com>
2020-11-27 20:31:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -25,4 +25,4 @@ def index():
     return render_template('index.html', url=url)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    app.run(host="ec2-13-232-115-29.ap-south-1.compute.amazonaws.com")
